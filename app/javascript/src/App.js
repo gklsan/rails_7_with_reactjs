c47da4afb9e7f6e90473c97258a04138732a151c
Gokul <pgokulmca@gmail.com>
2022-07-29 13:43:36 +0530

    ReactJS added part of the app javascript folder and gitignore added

---
+++ app/javascript/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
